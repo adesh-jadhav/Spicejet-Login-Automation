b212618f55fb41e8ef8deb9a1878bd3e6aa9798a
Adesh Jadhav <adesh.j7666@gmail.com>
2026-02-22 17:19:25 +0530

    Added WebDriverManager and headless support

--- src/test/java/UtilActions/SpicejetTestBase.java
+++ src/test/java/UtilActions/SpicejetTestBase.java
@@ -2,25 +2,29 @@ package UtilActions;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class SpicejetTestBase
+public class SpicejetTestBase {
 
+    public WebDriver obj1;
 
-{
-	public  WebDriver obj1;
+    public WebDriver WebDriverManager() {
 
-	public WebDriver WebDriverManager()
-	 
-	 
-	{
-		if(obj1==null)
-		{
-			System.setProperty("webdriver.chrome.driver", "F:\\Automation_Devolopment\\Selenium_Tool\\Selenium_Download\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
-			obj1 = new ChromeDriver();
-			obj1.manage().window().maximize();
-			obj1.get("https://www.spicejet.com/");
-		}
-		return obj1;
-		
-	}
+        if (obj1 == null) {
+
+            WebDriverManager.chromedriver().setup();
+
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+
+            obj1 = new ChromeDriver(options);
+            obj1.manage().window().maximize();
+            obj1.get("https://www.spicejet.com/");
+        }
+
+        return obj1;
+    }
 }
